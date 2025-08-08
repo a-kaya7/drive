808f6b6ae72303ec532c6330be9276772521dfdf
ahmet <ahmet@gmail.com>
2025-08-08 15:27:06 +0200

    Institut-List anzeigen

---
+++ Drive/src/main/java/net/drive/model/dto/administration/allgemein/InstitutListDTO.java
@@ -0,0 +1,11 @@
+package net.drive.model.dto.administration.allgemein;
+
+public record InstitutListDTO(
+		String institutsname,
+		String beschreibung,
+		String locale,
+		String telefon
+		
+		) {
+
+}
---
+++ Drive/src/main/java/net/drive/services/administration/allgemein/innensicht/InstitutListService.java
@@ -0,0 +1,32 @@
+package net.drive.services.administration.allgemein.innensicht;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.stereotype.Service;
+
+import net.drive.model.dto.administration.allgemein.InstitutListDTO;
+import net.drive.repository.administration.allgemein.IInstitutRepository;
+import net.drive.services.administration.allgemein.aussensicht.IInstitutListService;
+
+@Service
+public class InstitutListService implements IInstitutListService {
+	
+	private final IInstitutRepository institutRepo;
+	
+	public InstitutListService(IInstitutRepository institutRepo) {
+		this.institutRepo = institutRepo;
+	}
+
+	@Override
+	public List<InstitutListDTO> getAllInstitut() {
+	
+		return institutRepo.findAll().stream().map(entity -> new InstitutListDTO(
+				entity.getInstitutsname(),
+				entity.getBezeichnung(),
+				entity.getLocale(),
+				entity.getTelefon()
+				)).collect(Collectors.toList());
+	}
+
+}
